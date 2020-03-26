07aee16f852d35695770c8a07ca9ae10ccebb3b5
Willy <William.g.metcalf@gmail.com>
2020-03-26 11:11:30 -0500

    First Commit

--- DepartmentExercises/Controllers/EmployeeController.cs
+++ DepartmentExercises/Controllers/EmployeeController.cs
@@ -6,8 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using System.Data;
 using Microsoft.Data.SqlClient;
-using DepartmentExercises.Models;
 using Microsoft.AspNetCore.Http;
+using DepartmentExercises.Models;
 
 namespace DepartmentExercises.Controllers
 {
@@ -21,6 +21,7 @@ namespace DepartmentExercises.Controllers
         {
             _config = config;
         }
+
         public SqlConnection Connection
         {
             get
@@ -28,7 +29,7 @@ namespace DepartmentExercises.Controllers
                 return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             }
         }
-        
+
         [HttpGet]
         public async Task<IActionResult> Get()
         {
@@ -37,58 +38,29 @@ namespace DepartmentExercises.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"
-                        SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.Id, d.DeptName
-                        FROM Employee e
-                        LEFT JOIN Department d
-                        ON e.DepartmentId = d.Id";
-                    
+                    cmd.CommandText = "SELECT Id, FirstName, LastName, DepartmentId FROM Employee";
                     SqlDataReader reader = cmd.ExecuteReader();
                     List<Employee> employees = new List<Employee>();
-                    // The reader will read the returned data from the database if it finds the single row we're looking for. If it doesn't find the employee with the given Id, reader.Read() will return false
+
                     while (reader.Read())
                     {
-                        // Get ordinal returns us what "position" the Id column is in
-                        int idColumn = reader.GetOrdinal("Id");
-                        int idValue = reader.GetInt32(idColumn);
-
-                       
-                        int firstNameColumn = reader.GetOrdinal("FirstName");
-                        string firstNameValue = reader.GetString(firstNameColumn);
-
-                        int lastNameColumn = reader.GetOrdinal("LastName");
-                        string lastNameValue = reader.GetString(lastNameColumn);
-
-                        int departmentIdColumn = reader.GetOrdinal("DepartmentId");
-                        int departmentValue = reader.GetInt32(departmentIdColumn);
-
-                        int departmentNameColumn = reader.GetOrdinal("DeptName");
-                        string departmentNameValue = reader.GetString(departmentNameColumn);
-
-                       
-                        var employee = new Employee()
+                        Employee employee = new Employee
                         {
-                            Id = idValue,
-                            FirstName = firstNameValue,
-                            LastName = lastNameValue,
-                            DepartmentId = departmentValue,
-                            Department = new Department()
-                            {
-                                Id = departmentValue,
-                                DeptName = departmentNameValue
-                            }
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId"))
                         };
+
                         employees.Add(employee);
                     }
-                        // Now we can close the reader
-                        reader.Close();
+                    reader.Close();
 
-                        return Ok(employees);
-                    
-                    
+                    return Ok(employees);
                 }
             }
         }
+
         [HttpGet("{id}", Name = "GetEmployee")]
         public async Task<IActionResult> Get([FromRoute] int id)
         {
@@ -98,57 +70,143 @@ namespace DepartmentExercises.Controllers
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                        SELECT e.Id, e.FirstName, e.LastName, e.DepartmentId, d.Id, d.DeptName
-                        FROM Employee e
-                        LEFT JOIN Department d
-                        ON e.DepartmentId = d.Id
-                        WHERE e.Id = @id";
+                        SELECT Id, FirstName, LastName, DepartmentId FROM Employee
+                        WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = cmd.ExecuteReader();
 
+                    Employee employee = null;
+
+                    if (reader.Read())
+                    {
+                        employee = new Employee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId"))
+                        };
+                    }
+                    reader.Close();
+
+                    return Ok(employee);
+                }
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Employee employee)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Employee (FirstName, LastName, DepartmentId)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@firstName, @lastName, @departmentId)";
+                    cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                    cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                    cmd.Parameters.Add(new SqlParameter("@departmentId", employee.DepartmentId));
+                    int newId = (int)cmd.ExecuteScalar();
+                    employee.Id = newId;
+                    return CreatedAtRoute("GetEmployee", new { id = newId }, employee);
+                }
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] Employee employee)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"UPDATE Employee
+                                            SET FirstName = @firstName,
+                                                LastName = @lastName,
+                                                DepartmentId = @departmentId
+                                            WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@firstName", employee.FirstName));
+                        cmd.Parameters.Add(new SqlParameter("@lastName", employee.LastName));
+                        cmd.Parameters.Add(new SqlParameter("@departmentId", employee.DepartmentId));
+                        cmd.Parameters.Add(new SqlParameter("@id", employee.Id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            try
+            {
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Employee WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        int rowsAffected = cmd.ExecuteNonQuery();
+                        if (rowsAffected > 0)
+                        {
+                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+                        }
+                        throw new Exception("No rows affected");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (!EmployeeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+
+        private bool EmployeeExists(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT Id, FirstName, LastName, DepartmentId
+                        FROM Employee
+                        WHERE Id = @id";
                     cmd.Parameters.Add(new SqlParameter("@id", id));
 
                     SqlDataReader reader = cmd.ExecuteReader();
-                    List<Employee> employees = new List<Employee>();
-                    // The reader will read the returned data from the database if it finds the single row we're looking for. If it doesn't find the employee with the given Id, reader.Read() will return false
-                    while (reader.Read())
-                    {
-                        // Get ordinal returns us what "position" the Id column is in
-                        int idColumn = reader.GetOrdinal("Id");
-                        int idValue = reader.GetInt32(idColumn);
-
-
-                        int firstNameColumn = reader.GetOrdinal("FirstName");
-                        string firstNameValue = reader.GetString(firstNameColumn);
-
-                        int lastNameColumn = reader.GetOrdinal("LastName");
-                        string lastNameValue = reader.GetString(lastNameColumn);
-
-                        int departmentIdColumn = reader.GetOrdinal("DepartmentId");
-                        int departmentValue = reader.GetInt32(departmentIdColumn);
-
-                        int departmentNameColumn = reader.GetOrdinal("DeptName");
-                        string departmentNameValue = reader.GetString(departmentNameColumn);
-
-
-                        var employee = new Employee()
-                        {
-                            Id = idValue,
-                            FirstName = firstNameValue,
-                            LastName = lastNameValue,
-                            DepartmentId = departmentValue,
-                            Department = new Department()
-                            {
-                                Id = departmentValue,
-                                DeptName = departmentNameValue
-                            }
-                        };
-                        employees.Add(employee);
-                    }
-                    // Now we can close the reader
-                    reader.Close();
-
-                    return Ok(employees);
-
-
+                    return reader.Read();
                 }
             }
         }
